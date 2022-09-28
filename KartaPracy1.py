8f0655dfce3e90e559d43b36520e2db8e6b40065
23bp <114557740+PerzotProgramer@users.noreply.github.com>
2022-09-28 08:51:59 +0200

    Add files via upload

---
+++ KartaPracy1.py
@@ -0,0 +1,49 @@
+print("ZADANIE 1")
+a = int(input("Pierwsza Liczba: "))
+b = int(input("Druga Liczba: "))
+c = a**2+b**2
+print("wyjscie:",c)
+
+
+print("ZADANIE 2")
+a = int(input("Pierwsza Liczba: "))
+b = int(input("Druga Liczba: "))
+c = (a+b)**2
+print("wyjscie:",c)
+
+
+print("ZADANIE 3")
+a = int(input("Pierwsza Liczba: "))
+b = int(input("Druga Liczba: "))
+c = (a-b)**3
+print("wyjscie:",c)
+
+
+print("ZADANIE 4")
+a = int(input("Pierwsza Liczba: "))
+b = int(input("Druga Liczba: "))
+c = int(input("Trzecia Liczba: "))
+c = a*b*c
+print("wyjscie:",c)
+
+
+print("ZADANIE 5")
+a = float(input("Pierwsza Liczba: "))
+b = float(input("Druga Liczba: "))
+c = 2*(a+b)/5
+print("wyjscie1:",c)
+c = (a+b)*0.4
+print("wyjscie2:",c)
+
+
+print("ZADANIE 6")
+a = float(input("Brutto: "))
+c = a/1.23
+print("Netto:",c)
+
+
+print("ZADANIE 7")
+a = int(input("Pierwsza Liczba: "))
+b = int(input("Druga Liczba: "))
+c = a%b
+print("wyjscie:",c)
