0d5e49a06c3fdd13002ae617fcff964a19d76ac2
PerzotProgramer <bartoszperz2007@gmail.com>
2022-10-13 20:47:22 +0200

    Poprawiono zad3 (po raz 3)

--- KartaPracy2A.py
+++ KartaPracy2A.py
@@ -21,9 +21,9 @@ c = int(input("Liczba3: "))
 if a == b or a == c or b == c : 
     print("TAK, przynajmniej dwie są równe")
     if a == b and a == c and b == c : print("Wszystkie liczby są równe")
-    if a == b : print("są to: 'Liczba1' i 'Liczba2'")
-    if a == c : print("są to: 'Liczba1' i 'Liczba3'")
-    if b == c : print("są to: 'Liczba2' i 'Liczba3'")
+    elif a == b : print("są to: 'Liczba1' i 'Liczba2'")
+    elif a == c : print("są to: 'Liczba1' i 'Liczba3'")
+    elif b == c : print("są to: 'Liczba2' i 'Liczba3'")
 else : print("NIE, nie ma równych")
 
 #zad4
