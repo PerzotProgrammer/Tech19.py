a3c7e856f4d95b5ebf307d744ae428ddb2f5bf3d
PerzotProgramer <bartoszperz2007@gmail.com>
2022-10-15 18:30:45 +0200

    Dodano zad1,zad5 (bez rekurencji), poprawiono zad7

--- KartaPracy4.py
+++ KartaPracy4.py
@@ -1,8 +1,24 @@
-#TODO zad1
+#zad1
+def parzytstosc(x):
+    if x % 2 == 0: return True
+    else: return False
+
+def netto(x):
+    return x/1.23
+
+def pelnoletnosc(x):
+    if x >= 18 : return True
+    else : return False
+
+def mtf(x,p):
+    if (x ** p - x) % p == 0 : return True
+    else : return False
+
+def dzielniki(x):
+    for i in range (0,x) : 
+        if x % i == 0 : print(i)
 
 #zad2
-
-
 def dodaj(x,y):
     return x+y
 
@@ -16,8 +32,16 @@ def fib(x):
     if x == 1 or x == 2 : return 1
     else : return fib(x-1) + fib(x-2)
 
-#TODO zad5
-
+#TODO zad5 w rekurencji
+def fib2(x,a=1,b=1):
+    if x == 1 : return 1
+    else:
+        print(1)
+        print(1)
+        for i in range(x-2):
+            a, b = b, a + b
+            print(b)
+             
 #zad6
 def po(x,y):
     if y == 0 : return 1
@@ -34,7 +58,7 @@ def binar(x,numb = []):
     else : 
         zeroJeden = x % 2
         numb.append(zeroJeden)
-        x = floor(x/2)
+        x = x//2
         return binar(x)
 
 
@@ -51,6 +75,10 @@ print("Wynik:",silnia(a))
 print("ZADANIE 4")
 a = int(input("Który fib?: "))
 print(a,"liczba w ciągu to",fib(a))
+
+print("ZADANIE 5")
+a = int(input("Ile fibów?: "))
+fib2(a)
 
 print("ZADANIE 6")
 a = int(input("Podstawa: "))
