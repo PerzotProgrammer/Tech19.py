4d6bba96e7ef8bb93297ebbccbb44dc0f5b56adc
PerzotProgramer <bartoszperz2007@gmail.com>
2022-11-12 22:02:00 +0100

    Poprawiono zad 2 (Dzięki Radek <3)

--- POprSprawdziany/Spr1gr2.py
+++ POprSprawdziany/Spr1gr2.py
@@ -1,23 +1,23 @@
-# print("ZADANIE 1")
-# a = int(input())
-# b = int(input())
-# c = int(input())
+print("ZADANIE 1")
+a = int(input())
+b = int(input())
+c = int(input())
 
-# print("a: ",end="")
-# if c == b - a or c * 2 == b - a or c * 3 == b - a or c * 4 == b - a or c * 5 == b - a: print(True)
-# else: print(False)
+print("a: ",end="")
+if c == b - a or c * 2 == b - a or c * 3 == b - a or c * 4 == b - a or c * 5 == b - a: print(True)
+else: print(False)
 
-# print("b: ",end="")
-# if c >= b - a: print(1)
-# elif c * 2 >= b - a: print(2)
-# elif c * 3 >= b - a: print(3)
-# elif c * 4 >= b - a: print(4)
-# elif c * 5 >= b - a: print(5)
-# else: print(False)
+print("b: ",end="")
+if c >= b - a: print(1)
+elif c * 2 >= b - a: print(2)
+elif c * 3 >= b - a: print(3)
+elif c * 4 >= b - a: print(4)
+elif c * 5 >= b - a: print(5)
+else: print(False)
 
-# print("c: ",end="")
-# if c * 5 >= b - a: print(True,(b - a)/5)
-# else: print(False,(b - a)/5)
+print("c: ",end="")
+if c * 5 >= b - a: print(True,(b - a)/5)
+else: print(False,(b - a)/5)
 
 print("ZADANIE 2")
 for faj in range(1000,10000):
@@ -27,12 +27,12 @@ for faj in range(1000,10000):
     if jed  == dz * 2 and dz  == set * 2:
         print(faj)
 
-# print("ZADANIE 3")
-# x = 7
+print("ZADANIE 3")
+x = 7
 
-# for i in range(x):
-#     for j in range(x):
-#         if i + j == x or i + j + 2 == x or i + j + 1 == x: print("*",end="")
-#         elif i == j or i + 1 == j or j + 1 == i: print("*",end="")
-#         else: print(" ",end="")
-#     print("")
+for i in range(x):
+    for j in range(x):
+        if i + j == x or i + j + 2 == x or i + j + 1 == x: print("*",end="")
+        elif i == j or i + 1 == j or j + 1 == i: print("*",end="")
+        else: print(" ",end="")
+    print("")
