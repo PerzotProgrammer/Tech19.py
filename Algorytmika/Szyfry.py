c6b98e268210b144cd717ab135435bc4ea2571ec
Perzot Programista <bartoszperz2007@gmail.com>
2023-09-02 15:08:39 +0200

    Zmiana struktury repo

--- Algorytmika/Szyfry.py
+++
@@ -1,9 +0,0 @@
-# KODOWANIE HASHOWE
-from hashlib import md5, sha256, sha512
-input1 = input()
-
-print(sha512(input1.encode("utf-8")).hexdigest())
-
-# W PĘTLI
-for i in range(10000):
-    print(sha512(str(i).encode("utf-8")).hexdigest())
