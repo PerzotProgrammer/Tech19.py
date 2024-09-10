c6719f711e589e8f7967275e1bfb08a8569c925d
PerzotProgrammer <bartoszperz2007@gmail.com>
2024-09-10 11:16:04 +0200

    Dokończono zadania z lekcji

--- 3Klasa/Powtorka2Klasa/main.py
+++ 3Klasa/Powtorka2Klasa/main.py
@@ -27,8 +27,41 @@ def zad4():
     print(f'Liczba dzielników: {divs}')
 
 
+def zad5():
+    num = int(input('Podaj liczbę: '))
+    divs = sp.PrimeDividers(num)
+    print(f"Dzielniki pierwsze: {divs}")
+
+
+def zad6():
+    num1 = int(input('Podaj 1 liczbę: '))
+    num2 = int(input('Podaj 2 liczbę: '))
+    if sp.AreNumbersTwins(num1, num2):
+        print("Liczby są bliźniacze")
+    else:
+        print("Liczby nie są bliźniacze")
+
+
+def zad7():
+    num = int(input('Podaj liczbę: '))
+    if sp.IsPerfectNumber(num):
+        print("Jest to liczba doskonała")
+    else:
+        print("Nie jest to liczba doskonała")
+
+
 if __name__ == '__main__':
+    print("zad1")
     zad1()
+    print("zad2")
     zad2()
+    print("zad3")
     zad3()
+    print("zad4")
     zad4()
+    print("zad5")
+    zad5()
+    print("zad6")
+    zad6()
+    print("zad7")
+    zad7()
--- 3Klasa/Powtorka2Klasa/skryptyPowtorka.py
+++ 3Klasa/Powtorka2Klasa/skryptyPowtorka.py
@@ -4,7 +4,39 @@ def IsEven(num: int) -> bool:
 
 def Dividers(num: int) -> []:
     dividers = []
-    for i in range(2, num + 1):
+    for i in range(1, num + 1):
         if num % i == 0:
             dividers.append(i)
     return dividers
+
+
+def IsPrime(num: int) -> bool:
+    if num <= 2:
+        return False
+    for i in range(2, num):
+        if num % i == 0:
+            return False
+    return True
+
+
+def PrimeDividers(num: int) -> []:
+    dividers: [] = Dividers(num)
+    primeDividers = []
+    for divider in dividers:
+        if IsPrime(divider):
+            primeDividers.append(divider)
+    return primeDividers
+
+
+def AreNumbersTwins(a: int, b: int) -> bool:
+    if IsPrime(a) and IsPrime(b) and abs(a - b) == 2:
+        return True
+    return False
+
+
+def IsPerfectNumber(num: int) -> bool:
+    dividers: [] = Dividers(num)
+    dividers.remove(num)
+    if sum(dividers) == num:
+        return True
+    return False
