c41c7ce1e50a4cc12d953576ffedf12b6e29320f
PerzotProgrammer <bartoszperz2007@gmail.com>
2024-10-01 11:16:25 +0200

    Przesłano zadania z lekcji

--- 3Klasa/SitoEratostenesa/main.py
+++ 3Klasa/SitoEratostenesa/main.py
@@ -21,6 +21,35 @@ def zad2():
     print(f"Jest {numOfPrimes} liczb pierwszych a ich suma to {sumOfPrimes}")
 
 
+def zad4():
+    with open("liczby.txt") as file:
+        numbers = []
+        for line in file:
+            numbers.append(int(line))
+        primes = soe.SieveOfEratostenes(max(numbers) + 1)
+        count = 0
+        for number in numbers:
+            if primes[number]:
+                count += 1
+        print(f"W pliku jest {count} liczb pierwszych")
+        file.close()
+
+
+def zad5():
+    num1 = int(input("Podaj 1 liczbę: "))
+    num2 = int(input("Podaj 2 liczbę: "))
+    low = min(num1, num2)
+    high = max(num1, num2)
+    primes = soe.SieveOfEratostenes(high + 1)
+    numbers = []
+    for i in range(low, high + 1):
+        if primes[i]:
+            numbers.append(i)
+    print(f"Liczby pierwsze z przedziału <{low} ; {high}> - {numbers}, jest ich {len(numbers)} a ich suma to {sum(numbers)}")
+
+
 if __name__ == '__main__':
     zad1()
     zad2()
+    zad4()
+    zad5()
